5e3caff6e81d3f8b859afc50726170d984f4e3ce
Akira-Arch <akira.archangel@gmail.com>
2022-02-15 20:13:48 -0600

    created posts and added eslint rules

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '.';
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { PostCard, PostWidget, Categories } from '../components';
+import { getPosts } from '../services';
 
-export default function Home() {
+export default function Home({ posts }: { posts: any }) {
   return (
-    <div className="container">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
-        <title>Create Next App</title>
+        <title>cms blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post: any) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {
+      posts,
+    },
+  };
+};
